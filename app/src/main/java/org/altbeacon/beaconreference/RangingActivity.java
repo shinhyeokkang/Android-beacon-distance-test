1339809683cb4025fe1ed47709dfc63b602cac33
shinhyeokkang <ksh84610@gmail.com>
2021-02-15 19:57:30 +0900

    debug test methods

--- app/src/main/java/org/altbeacon/beaconreference/RangingActivity.java
+++ app/src/main/java/org/altbeacon/beaconreference/RangingActivity.java
@@ -1,6 +1,11 @@
 package org.altbeacon.beaconreference;
 
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.FileOutputStream;
+import java.io.IOException;
 import java.util.Collection;
+import java.util.LinkedList;
 
 import android.app.Activity;
 
@@ -8,6 +13,7 @@ import android.os.Bundle;
 import android.os.RemoteException;
 import android.util.Log;
 import android.widget.EditText;
+import android.widget.Toast;
 
 import org.altbeacon.beacon.AltBeacon;
 import org.altbeacon.beacon.Beacon;
@@ -16,16 +22,57 @@ import org.altbeacon.beacon.BeaconManager;
 import org.altbeacon.beacon.BeaconParser;
 import org.altbeacon.beacon.RangeNotifier;
 import org.altbeacon.beacon.Region;
+import org.apache.poi.hssf.usermodel.HSSFWorkbook;
+import org.apache.poi.ss.usermodel.Cell;
+import org.apache.poi.ss.usermodel.Row;
+import org.apache.poi.ss.usermodel.Sheet;
+import org.apache.poi.ss.usermodel.Workbook;
 
 public class RangingActivity extends Activity implements BeaconConsumer {
     protected static final String TAG = "RangingActivity";
     private BeaconManager beaconManager = BeaconManager.getInstanceForApplication(this);
+    private int[] rssiList = new int[100];
+    private int Tx;
+    private int count=0;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_ranging);
     }
+
+//    private void saveExcel() throws IOException { // rssiList를 받아 엑셀파일로 작성
+//        Workbook workbook = new HSSFWorkbook();
+//
+//        Sheet sheet = workbook.createSheet(); // 새로운 시트 생성
+//
+//        Row row = sheet.createRow(0); // 새로운 행 생성
+//        Cell cell;
+//
+//        cell = row.createCell(0); // 1번 셀 생성
+//        cell.setCellValue("TxPower"); // 1번 셀 값 입력
+//
+//        cell = row.createCell(1); // 2번 셀 생성
+//        cell.setCellValue("RSSI"); // 2번 셀 값 입력
+//
+//        for(int i = 0; i < rssiList.length ; i++){ // 데이터 엑셀에 입력
+//            row = sheet.createRow(i+1);
+//            cell = row.createCell(0);
+//            cell.setCellValue(Tx);
+//            cell = row.createCell(1);
+//            cell.setCellValue(rssiList[i]);
+//        }
+//
+//        File xlsFile = new File(getExternalFilesDir(null),"test.xls");
+//        try{
+//            FileOutputStream os = new FileOutputStream(xlsFile);
+//            workbook.write(os); // 외부 저장소에 엑셀 파일 생성
+//        }catch (IOException e){
+//            e.printStackTrace();
+//        }
+//        Toast.makeText(getApplicationContext(),xlsFile.getAbsolutePath()+"에 저장되었습니다",Toast.LENGTH_SHORT).show();
+//    }
+
 
     @Override 
     protected void onDestroy() {
@@ -53,7 +100,18 @@ public class RangingActivity extends Activity implements BeaconConsumer {
               if (beacons.size() > 0) {
                   Log.d(TAG, "didRangeBeaconsInRegion called with beacon count:  "+beacons.size());
                   Beacon firstBeacon = beacons.iterator().next();
+                  //Tx = firstBeacon.getTxPower();
                   logToDisplay("The first beacon " + firstBeacon.toString() + " is about " + firstBeacon.getDistance() + " meters away. \n" +"Rssi: " + firstBeacon.getRssi() + "TxPower: " + firstBeacon.getTxPower());
+//                  rssiList[count] = firstBeacon.getRssi();
+//                  if(count==100){
+//                      try {
+//                          saveExcel();
+//                      } catch (IOException e) {
+//                          e.printStackTrace();
+//                      }
+//                      logToDisplay("List has filled!!!!!!!!!!!!"); // 완성 문구 출력
+//                  }
+//                  count++;
               }
            }
 
